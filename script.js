997908d7343d6bd35675eb633a282fe3e68fd5d2
Hamukaze <maplecow@gmail.com>
2022-06-27 18:11:07 +0900

    Added functions to add books to html

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+const libraryWrapper = document.getElementById("library");
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -7,13 +8,50 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
+function renderLibrary(library){
+    for(object in library){
+        renderBook(object);
+    }
+}
+
+function renderBook(object){
+    const book = document.createElement('div');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const read = document.createElement('p');
+    book.setAttribute('class', 'book');
+    title.setAttribute('class', 'title');
+    author.setAttribute('class', 'author');
+    pages.setAttribute('class', 'pages');
+    read.setAttribute('class', 'read');
+    title.innerHTML = object.title;
+    author.innerHTML = object.author;
+    pages.innerHTML = object.pages;
+    switch (object.read){
+        case "planToRead":
+            read.innerHTML = "Plan To Read";
+            break;
+        case "inProgress":
+            read.innerHTML = "In Progress";
+            break;
+        case "completed":
+            read.innerHTML = "Completed";
+            break;
+    }
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pages);
+    book.appendChild(read);
+    libraryWrapper.appendChild(book);
+}
+
 function addBookToLibrary() {
     title = document.getElementById("title").value;
     author = document.getElementById("author").value;
     pages = document.getElementById("pages").value;
     read = document.getElementById("read").value;
     let newBook = new Book(title,author,pages,read);
-    console.log(newBook);
     myLibrary.push(newBook);
-    console.log(myLibrary);
+    renderBook(newBook);
 }
