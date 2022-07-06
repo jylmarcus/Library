5e3a184bc57084b060c0446347bf3a37107556ce
Hamukaze <maplecow@gmail.com>
2022-07-06 16:36:21 +0900

    Added feature to open edit book modal

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ function renderBook(object, index){
     const editImg = document.createElement('img');
     const delImg = document.createElement('img');
     const library = document.querySelector('.library');
+
     book.setAttribute('class', 'book');
     entry.setAttribute('class', 'entry');
     title.setAttribute('class', 'title');
@@ -34,6 +35,9 @@ function renderBook(object, index){
     pages.setAttribute('class', 'pages');
     read.setAttribute('class', 'read');
     edit.setAttribute('class', 'edit');
+    editImg.setAttribute('data-modal-open', '#editModal')
+    delImg.setAttribute('data-modal-delete', '#delModal')
+
     entry.innerHTML = index;
     title.innerHTML = object.title;
     author.innerHTML = object.author;
@@ -51,6 +55,7 @@ function renderBook(object, index){
             read.innerHTML = "Completed";
             break;
     }
+
     edit.appendChild(editImg);
     edit.appendChild(delImg);
     book.appendChild(entry);
@@ -59,6 +64,7 @@ function renderBook(object, index){
     book.appendChild(pages);
     book.appendChild(read);
     book.appendChild(edit);
+
     book.addEventListener('mouseenter', ()=>{
         book.classList.add('hover');
         editImg.src = "./img/pencil-box-outline.svg";
@@ -69,6 +75,36 @@ function renderBook(object, index){
         editImg.src = "./img/pencil-box.svg";
         delImg.src = "./img/trash-can.svg";
     })
+
+    editImg.addEventListener('click', ()=>{
+        const modal = document.querySelector(editImg.dataset.modalOpen);
+        openEditModal(modal, title, author, pages, read);
+
+        function openEditModal(modal, title, author, pages, read){
+            if (modal == null) return;
+            modal.classList.add('active');
+            overlay.classList.add('active');
+            const titleInput = document.querySelector('#editTitle');
+            const authorInput = document.querySelector('#editAuthor');
+            const pagesInput = document.querySelector('#editPages');
+            const readInput = document.querySelector('#editRead');
+            titleInput.defaultValue = title.innerHTML;
+            authorInput.defaultValue = author.innerHTML;
+            pagesInput.defaultValue = pages.innerHTML;
+            switch (read.innerHTML){
+                case "Plan To Read":
+                    readInput.value = 'planToRead';
+                    break;
+                case "In Progress":
+                    readInput.value = 'inProgress';
+                    break;
+                case "Completed":
+                    readInput.value = 'completed';
+                    break;
+            }
+        }
+    })
+
     library.appendChild(book);
 }
 
