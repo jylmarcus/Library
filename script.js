0cd30ccb1528a68e9309bf384c00a0a5fd5ef3ce
Hamukaze <maplecow@gmail.com>
2022-07-10 16:43:33 +0900

    Reworked create and update functionality

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
 let myLibrary = [];
-const libraryWrapper = document.getElementById("library");
+var currentBook = null;
+var currentBookDiv = null;
+var modalHeader = document.querySelector('.modalHeaderText');
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+function onSubmit() {
+    let formData = readFormData();
+    if (currentBook == null){
+        addBookToLibrary(formData);
+    } else {
+        editBook(formData);
+    }
+    currentBook = null;
+    currentBookDiv = null;
+    const form = document.getElementById('bookForm');
+    form.reset();
+}
+
+function readFormData() {
+    let formData = {};
+    formData["title"] = document.getElementById('title').value;
+    formData["author"] = document.getElementById('author').value;
+    formData["pages"] = document.getElementById('pages').value;
+    formData["read"] = document.getElementById('read').value;
+    return formData;
+}
+
+function Book(data) {
+    this.title = data.title
+    this.author = data.author
+    this.pages = data.pages
+    this.read = data.read
+}
+
+function addBookToLibrary(data) {
+
+    myLibrary.push(new Book(data));
+    let index = myLibrary.length;
+    renderBook(data, index);
+
+    const modal = document.querySelector(".modal");
+    closeModal(modal);
 }
 
 function renderLibrary(library){
@@ -16,7 +50,11 @@ function renderLibrary(library){
     }
 }
 
-function renderBook(object, index){
+function getBookByIndex(index){
+    return myLibrary[index];
+}
+
+function renderBook(data, index){
     const book = document.createElement('div');
     const entry = document.createElement('p');
     const title = document.createElement('p');
@@ -29,6 +67,7 @@ function renderBook(object, index){
     const library = document.querySelector('.library');
 
     book.setAttribute('class', 'book');
+    book.setAttribute('id', `${index}`);
     entry.setAttribute('class', 'entry');
     title.setAttribute('class', 'title');
     author.setAttribute('class', 'author');
@@ -39,12 +78,10 @@ function renderBook(object, index){
     delImg.setAttribute('data-modal-delete', '#delModal')
 
     entry.innerHTML = index;
-    title.innerHTML = object.title;
-    author.innerHTML = object.author;
-    pages.innerHTML = object.pages;
-    editImg.src = "./img/pencil-box.svg";
-    delImg.src = "./img/trash-can.svg";
-    switch (object.read){
+    title.innerHTML = data.title;
+    author.innerHTML = data.author;
+    pages.innerHTML = data.pages;
+    switch (data.read){
         case "planToRead":
             read.innerHTML = "Plan To Read";
             break;
@@ -55,6 +92,10 @@ function renderBook(object, index){
             read.innerHTML = "Completed";
             break;
     }
+
+    editImg.src = "./img/pencil-box.svg";
+    delImg.src = "./img/trash-can.svg";
+    
 
     edit.appendChild(editImg);
     edit.appendChild(delImg);
@@ -77,58 +118,64 @@ function renderBook(object, index){
     })
 
     editImg.addEventListener('click', ()=>{
-        const modal = document.querySelector(editImg.dataset.modalOpen);
-        openEditModal(modal, title, author, pages, read);
-
-        function openEditModal(modal, title, author, pages, read){
-            if (modal == null) return;
-            modal.classList.add('active');
-            overlay.classList.add('active');
-
-            const titleInput = document.querySelector('#editTitle');
-            const authorInput = document.querySelector('#editAuthor');
-            const pagesInput = document.querySelector('#editPages');
-            const readInput = document.querySelector('#editRead');
-            
-            titleInput.defaultValue = title.innerHTML;
-            authorInput.defaultValue = author.innerHTML;
-            pagesInput.defaultValue = pages.innerHTML;
-            switch (read.innerHTML){
-                case "Plan To Read":
-                    readInput.value = 'planToRead';
-                    break;
-                case "In Progress":
-                    readInput.value = 'inProgress';
-                    break;
-                case "Completed":
-                    readInput.value = 'completed';
-                    break;
-            }
-        }
+        const modal = document.querySelector('.modal');
+        modalHeader.innerHTML = 'Edit book';
+        currentBook = myLibrary[index-1];
+        console.log(currentBook);
+        currentBookDiv = book;
+        openEditModal(modal, currentBook);
     })
 
     library.appendChild(book);
 }
 
-function addBookToLibrary() {
-    title = document.getElementById("title").value;
-    author = document.getElementById("author").value;
-    pages = document.getElementById("pages").value;
-    read = document.getElementById("read").value;
+function openEditModal(modal, currentBook){
+    if (modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
 
-    let newBook = new Book(title,author,pages,read);
-    myLibrary.push(newBook);
-    let index = myLibrary.length;
-    renderBook(newBook, index);
-
-    const modal = document.querySelector(".closeNewBook").closest('.modal');
-    closeModal(modal);
-
-    const form = document.getElementById('newBook');
-    form.reset();
+    titleInput = document.getElementById('title');
+    authorInput = document.getElementById('author');
+    pagesInput = document.getElementById('pages');
+    readInput = document.getElementById('read');
+    
+    titleInput.value = currentBook.title;
+    authorInput.value = currentBook.author;
+    pagesInput.value = currentBook.pages;
+    readInput.value = currentBook.read;
 }
 
-const openModalButton = document.querySelector('[data-modal-target]');
+function editBook(data){
+    title = currentBookDiv.children[1];
+    author = currentBookDiv.children[2];
+    pages = currentBookDiv.children[3];
+    read = currentBookDiv.children[4];
+
+    title.innerHTML = data.title;
+    author.innerHTML = data.author;
+    pages.innerHTML = data.pages;
+    switch (data.read){
+        case "planToRead":
+            read.innerHTML = "Plan To Read";
+            break;
+        case "inProgress":
+            read.innerHTML = "In Progress";
+            break;
+        case "completed":
+            read.innerHTML = "Completed";
+            break;
+    }
+
+    currentBook.title = data.title;
+    currentBook.author = data.author;
+    currentBook.pages = data.pages;
+    currentBook.read = data.read;
+
+    const modal = document.querySelector(".modal")
+    closeModal(modal);
+}
+
+const addBookButton = document.querySelector('.addBookButton');
 const closeModalButton = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
 
@@ -139,8 +186,9 @@ overlay.addEventListener('click', () => {
     })
 })
 
-openModalButton.addEventListener('click',()=> {
-    const modal = document.querySelector(openModalButton.dataset.modalTarget);
+addBookButton.addEventListener('click',()=> {
+    const modal = document.querySelector('.modal');
+    modalHeader.innerHTML = 'Add a book';
     openModal(modal);
 })
 
